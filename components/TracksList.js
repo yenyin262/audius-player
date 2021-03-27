451e37e8e6976d9c180fb0b6da8a14a205b478b1
yenyin262 <yenyinf287@gmail.com>
2021-03-26 21:10:48 -0700

    style tracklist

--- components/TracksList.js
+++ components/TracksList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ScrollView } from "react-native";
 import {
   SafeAreaView,
   View,
@@ -7,33 +8,50 @@ import {
   Text,
   StatusBar,
   TouchableOpacity,
+  Image,
 } from "react-native";
 
-const Item = ({ id, title, onPress }) => {
+const Item = ({ id, title, onPress, artwork, name }) => {
   function onTrackClick() {
     onPress(id);
   }
 
   return (
     <TouchableOpacity style={styles.item} onPress={onTrackClick}>
-      <Text style={styles.title}>{title}</Text>
+      <Image source={{ uri: artwork }} style={{ width: 80, height: 80 }} />
+      <View style={styles.contentContainer}>
+        <Text style={styles.title} ellipsizeMode="tail" numberOfLines={1}>
+          {title}
+        </Text>
+        <Text style={styles.name} ellipsizeMode="tail" numberOfLines={1}>
+          {name}
+        </Text>
+      </View>
     </TouchableOpacity>
   );
 };
 
 const TracksList = ({ tracks, onPress }) => {
+  console.log(tracks, "find tracks ");
+
   const renderItem = ({ item }) => (
-    <Item id={item.id} title={item.title} onPress={onPress} />
+    <Item
+      id={item.id}
+      title={item.title}
+      onPress={onPress}
+      artwork={item.artwork["480x480"]}
+      name={item.user.name}
+    />
   );
 
   return (
-    <SafeAreaView style={styles.container}>
+    <ScrollView style={styles.container}>
       <FlatList
         data={tracks}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
       />
-    </SafeAreaView>
+    </ScrollView>
   );
 };
 
@@ -41,15 +59,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: "black",
+    width: "100%",
+  },
+
+  contentContainer: {
+    flex: 1,
+    width: "100%",
+    paddingHorizontal: 10,
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
   },
   item: {
-    backgroundColor: "#f9c2ff",
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+    marginVertical: 5,
+    paddingHorizontal: 10,
+    flex: 1,
+    flexDirection: "row",
+    width: "100%",
   },
   title: {
-    fontSize: 32,
+    fontSize: 20,
+    color: "white",
+    textAlign: "left",
+    textTransform: "capitalize",
+    marginBottom: 5,
+  },
+
+  name: {
+    fontSize: 18,
+    fontWeight: "400",
+    color: "#999A9E",
+    textTransform: "capitalize",
   },
 });
 
