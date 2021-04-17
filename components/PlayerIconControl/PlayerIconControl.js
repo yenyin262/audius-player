53489ca3e15f1da5514df903aeaed62291fb18d1
alarivan <alarivan@pm.me>
2021-04-17 16:52:55 -0700

    Remove unsued styles and props

--- components/PlayerIconControl/PlayerIconControl.js
+++ components/PlayerIconControl/PlayerIconControl.js
@@ -1,12 +1,6 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function PlayerIconControl(props) {
-  const { onIconPress, onValuePress, value, icon: Icon } = props;
-  return (
-    <View style={{ flex: 1, flexDirection: "row", alignItems: "center" }}>
-      <Icon size={40} color="white" onPress={onIconPress} />
-    </View>
-  );
+  const { icon: Icon } = props;
+  return <Icon size={40} color="white" />;
 }
