69e39d0883de1632c3ece80b6ab6fc6e7b939848
alarivan <alarivan@pm.me>
2021-04-18 11:29:26 -0700

    Add position and duration to pLayer screen

--- screen/PlayerScreen/PlayerScreen.js
+++ screen/PlayerScreen/PlayerScreen.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, Text, Image, Dimensions } from "react-native";
+import React, { useEffect, useMemo } from "react";
+import { View, Text, Image, Dimensions, StyleSheet } from "react-native";
 import ButtonIcon from "../../components/ButtonIcon/ButtonIcon";
 import { MaterialIcons, Ionicons, Entypo } from "@expo/vector-icons";
 import { usePlayer } from "../../context/player";
@@ -8,8 +8,41 @@ import PlayerIconControl from "../../components/PlayerIconControl/PlayerIconCont
 const { width } = Dimensions.get("window");
 const imageSize = width - 96;
 
+function getInitialPosition(duration) {
+  if (duration > 3600) return "0:00:00";
+  else if (duration > 600) return "00:00";
+  return "0:00";
+}
+
 export default function PlayerScreen({}) {
-  const { track, isPlaying, toggle } = usePlayer();
+  const { track, isPlaying, sound, toggle } = usePlayer();
+  const remaining = useMemo(
+    () => new Date(track.duration * 1000).toISOString().substr(15, 4),
+    [track.duration]
+  );
+  const [position, setPosition] = React.useState(
+    getInitialPosition(track.duration)
+  );
+
+  useEffect(() => {
+    setPosition(getInitialPosition(track.duration));
+  }, [track.duration]);
+
+  useEffect(() => {
+    if (sound) {
+      sound.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
+    }
+  }, [sound]);
+
+  /**
+   * @param {import("expo-av").AVPlaybackStatus} status
+   */
+  function onPlaybackStatusUpdate(status) {
+    if (status.isPlaying) {
+      setPosition(new Date(status.positionMillis).toISOString().substr(15, 4));
+    }
+  }
+
   return (
     <View
       style={{
@@ -60,7 +93,13 @@ export default function PlayerScreen({}) {
             {track.user.name}
           </Text>
         </View>
-
+        <View>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={styles.text}>{position}</Text>
+            <View style={{ flex: 1 }} />
+            <Text style={[styles.text, { width: 40 }]}>{remaining}</Text>
+          </View>
+        </View>
         <View
           style={{
             flex: 2,
@@ -139,3 +178,28 @@ export default function PlayerScreen({}) {
     </View>
   );
 }
+const styles = StyleSheet.create({
+  slider: {
+    marginTop: -12,
+  },
+  container: {
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingTop: 16,
+  },
+  track: {
+    height: 2,
+    borderRadius: 1,
+  },
+  thumb: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: "white",
+  },
+  text: {
+    color: "rgba(255, 255, 255, 0.72)",
+    fontSize: 12,
+    textAlign: "center",
+  },
+});
