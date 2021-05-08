a4dc3e51390109a16b19880ad958d72c458f04ae
alarivan <alarivan@pm.me>
2021-05-08 13:21:33 -0700

    Add real user profile data and fix styles

--- screen/UserProfile/UserProfileScreen.js
+++ screen/UserProfile/UserProfileScreen.js
@@ -1,38 +1,59 @@
 //import liraries
-import React, { Component } from "react";
+import React from "react";
 import {
   View,
   Text,
   StyleSheet,
   Image,
-  Dimensions,
   ImageBackground,
+  ScrollView,
 } from "react-native";
 
 import { usePlayer } from "../../context/player";
 import { MaterialIcons } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import ButtonIcon from "../../components/ButtonIcon/ButtonIcon";
-import { Ionicons } from "@expo/vector-icons";
 import TracksList from "../../components/TracksList/TracksList";
 
-const { width } = Dimensions.get("window");
-const imageSize = width - 300;
+import { useQuery } from "react-query";
+import { getUserProfile } from "../../services/audius";
 
 // create a component
-const UserProfileScreen = ({ navigation }) => {
+const UserProfileScreen = ({ route, navigation }) => {
+  const userId = route?.params?.userId || "nlGNe";
+
+  const { isLoading, isError, data: user, error } = useQuery(
+    ["user:profile", userId],
+    () => getUserProfile(userId)
+  );
+
   const { track, play } = usePlayer();
-  console.log(track, "profile track 1");
+  const tracks = track ? [track] : [];
+  console.log(user, "user data");
 
   const navigateToPlayer = (track) => {
     navigation.navigate("PlayerScreen");
     play(track);
   };
+
+  if (isLoading) {
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Loading</Text>
+    </View>;
+  }
+
+  if (!user) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>User not found</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <View style={{ flex: 1 }}>
+    <ScrollView style={styles.container}>
+      <View style={styles.banner}>
         <ImageBackground
-          source={{ uri: track.user.cover_photo["640x"] }}
+          source={{ uri: user.cover_photo["640x"] }}
           style={{
             flexDirection: "row",
             resizeMode: "cover",
@@ -42,54 +63,68 @@ const UserProfileScreen = ({ navigation }) => {
           }}
         />
         <Image
-          source={{ uri: track.user.profile_picture["480x480"] }}
+          source={{ uri: user.profile_picture["480x480"] }}
           style={{
-            width: imageSize,
-            height: imageSize,
+            width: 100,
+            height: 100,
             borderRadius: 150,
             position: "absolute",
-            top: 70,
+            left: 20,
+            bottom: -50,
           }}
         />
+      </View>
+      <View style={styles.profile}>
         <View
           style={{
             marginVertical: 10,
             flexDirection: "row",
             justifyContent: "space-between",
-            paddingTop: 80,
           }}
         >
           <View>
-            <Text style={{ color: "white", fontSize: 18 }}>
-              {track.user.handle}{" "}
+            <Text
+              style={{
+                color: "white",
+                fontSize: 16,
+                fontWeight: "bold",
+                marginBottom: 5,
+              }}
+            >
+              {user.handle}
             </Text>
-            <Text style={{ color: "white", fontSize: 18, marginVertical: 10 }}>
-              @{track.user.handle}{" "}
+            <Text
+              style={{
+                color: "white",
+                fontSize: 14,
+              }}
+            >
+              @{user.handle}
             </Text>
           </View>
           <View
             style={{
               flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
               borderColor: "grey",
               borderWidth: 2,
               borderRadius: 10,
-              height: 60,
-              width: 150,
-              paddingHorizontal: 20,
+              height: 40,
               paddingVertical: 10,
+              paddingHorizontal: 20,
             }}
           >
-            <MaterialIcons name="person-add-alt-1" size={30} color="white" />
+            <MaterialIcons name="person-add-alt-1" size={24} color="white" />
             <Text
               style={{
                 color: "white",
-                fontSize: 18,
+                fontSize: 14,
                 textTransform: "uppercase",
                 fontWeight: "800",
-                paddingTop: 5,
+                marginLeft: 10,
               }}
             >
-              {" "}
               Follow
             </Text>
           </View>
@@ -103,26 +138,24 @@ const UserProfileScreen = ({ navigation }) => {
         >
           <Text style={{ color: "white", fontSize: 18, marginRight: 5 }}>
             <Text style={{ fontWeight: "900", fontSize: 24 }}>
-              {" "}
-              {track.user.track_count}{" "}
+              {user.track_count}{" "}
             </Text>
-            {track.user.track_count > 1 ? "tracks" : "track"}
+            {user.track_count > 1 ? "tracks" : "track"}
           </Text>
           <Text style={{ color: "white", fontSize: 18, marginRight: 5 }}>
             <Text style={{ fontWeight: "900", fontSize: 24 }}>
-              {track.user.follower_count}{" "}
+              {user.follower_count}{" "}
             </Text>
             Followers
           </Text>
           <Text style={{ color: "white", fontSize: 18, marginRight: 5 }}>
             <Text style={{ fontWeight: "900", fontSize: 24 }}>
-              {" "}
-              {track.user.followee_count}{" "}
+              {user.followee_count}{" "}
             </Text>
             Following
           </Text>
         </View>
-        <View style={{ marginVertical: 10 }}>
+        <View style={styles.bio}>
           <Text
             style={{
               color: "white",
@@ -131,142 +164,81 @@ const UserProfileScreen = ({ navigation }) => {
               marginHorizontal: 5,
             }}
           >
-            {track.user.bio}{" "}
+            {user.bio}
           </Text>
         </View>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-          }}
-        >
-          <View style={{ alignItems: "center" }}>
-            <MaterialCommunityIcons name="music" size={40} color="white" />
-            <Text
-              style={{
-                color: "white",
-                fontSize: 18,
-                marginRight: 5,
-                textTransform: "uppercase",
-                fontWeight: "700",
-              }}
-            >
-              Music
-            </Text>
-          </View>
-          <View style={{ alignItems: "center" }}>
-            <MaterialIcons name="album" size={40} color="white" />
-            <Text
-              style={{
-                color: "white",
-                fontSize: 18,
-                marginRight: 5,
-                textTransform: "uppercase",
-                fontWeight: "700",
-              }}
-            >
-              Albums
-            </Text>
-          </View>
-          <View style={{ alignItems: "center" }}>
-            <MaterialCommunityIcons
-              name="playlist-music"
-              size={40}
-              color="white"
-            />
-            <Text
-              style={{
-                color: "white",
-                fontSize: 18,
-                marginRight: 5,
-                textTransform: "uppercase",
-                fontWeight: "700",
-              }}
-            >
-              Playlists
-            </Text>
-          </View>
-          <View style={{ alignItems: "center" }}>
-            <MaterialIcons name="repeat" size={40} color="white" />
-            <Text
-              style={{
-                color: "white",
-                fontSize: 18,
-                marginRight: 5,
-                textTransform: "uppercase",
-                fontWeight: "700",
-              }}
-            >
-              Reposts
-            </Text>
-          </View>
-        </View>
-        <View style={{ marginVertical: 20 }}>
-          <TracksList tracks={[track]} onPress={navigateToPlayer} />
-          {/* <View style={{ flexDirection: "row" }}>
-            <Image
-              source={{ uri: track.artwork["480x480"] }}
-              style={{
-                width: imageSize,
-                height: imageSize,
-              }}
-            />
-            <View style={{ width: 280, marginHorizontal: 20 }}>
-              <Text
-                style={{
-                  color: "white",
-                  textAlign: "right",
-                  paddingRight: 50,
-                  marginVertical: 10,
-                }}
-              >
-                {new Date(track.duration * 1000).toISOString().substr(15, 4)}
-              </Text>
-              <Text style={{ color: "white" }}>{track.title}</Text>
-              <Text style={{ color: "white", marginTop: 10 }}>
-                {track.user.handle}
-              </Text>
-            </View>
-          </View>
-          <View
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <View style={{ alignItems: "center" }}>
+          <MaterialCommunityIcons name="music" size={40} color="white" />
+          <Text
             style={{
-              marginVertical: 20,
-              flexDirection: "row",
-              alignItems: "center",
+              color: "white",
+              fontSize: 18,
+              marginRight: 5,
+              textTransform: "uppercase",
+              fontWeight: "700",
             }}
           >
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "flex-start",
-                width: 200,
-              }}
-            >
-              <ButtonIcon
-                icon={(props) => (
-                  <MaterialIcons name="repeat" size={40} color="white" />
-                )}
-                value={track.repost_count}
-              />
-
-              <ButtonIcon
-                icon={(props) => (
-                  <MaterialCommunityIcons
-                    name="cards-heart"
-                    size={24}
-                    color="white"
-                  />
-                )}
-                value={track.favorite_count}
-              />
-            </View>
-            <Text style={{ color: "white", paddingLeft: 120 }}>
-              {track.play_count} plays
-            </Text>
-          </View> */}
+            Music
+          </Text>
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <MaterialIcons name="album" size={40} color="white" />
+          <Text
+            style={{
+              color: "white",
+              fontSize: 18,
+              marginRight: 5,
+              textTransform: "uppercase",
+              fontWeight: "700",
+            }}
+          >
+            Albums
+          </Text>
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <MaterialCommunityIcons
+            name="playlist-music"
+            size={40}
+            color="white"
+          />
+          <Text
+            style={{
+              color: "white",
+              fontSize: 18,
+              marginRight: 5,
+              textTransform: "uppercase",
+              fontWeight: "700",
+            }}
+          >
+            Playlists
+          </Text>
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <MaterialIcons name="repeat" size={40} color="white" />
+          <Text
+            style={{
+              color: "white",
+              fontSize: 18,
+              marginRight: 5,
+              textTransform: "uppercase",
+              fontWeight: "700",
+            }}
+          >
+            Reposts
+          </Text>
         </View>
       </View>
-    </View>
+      <View style={{ marginVertical: 20, paddingBottom: 40 }}>
+        <TracksList tracks={tracks} onPress={navigateToPlayer} />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -274,13 +246,18 @@ const UserProfileScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
-    marginTop: 50,
     paddingTop: 40,
-    paddingLeft: 15,
-    paddingRight: 15,
     backgroundColor: "black",
   },
+  banner: {
+    marginBottom: 50,
+  },
+  profile: {
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+  bio: { marginVertical: 10 },
+  content: {},
 });
 
 //make this component available to the app
